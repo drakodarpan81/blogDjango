7b425d6ffee2f5299635695982d6d609b2c1e7cb
drakodarpan81 <drakodarpan@gmail.com>
2022-04-06 12:14:22 -0600

    Validación de información - 31

--- aplicaciones/blog/urls.py
+++ aplicaciones/blog/urls.py
@@ -5,4 +5,10 @@ from .views import *
 
 urlpatterns = [
     path('', Home, name='index'),
+    path('generales/', generales, name='generales'),
+    path('programacion/', programacion, name='programacion'),
+    path('tecnologia/', tecnologia, name='tecnologia'),
+    path('tutoriales/', tutoriales, name='tutoriales'),
+    path('video_juegos/', video_juegos, name='video_juegos'),
+    path('<slug:slug>/', detallePost, name='detalle_post'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -1,5 +1,79 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
 
 def Home(request):
-    return render(request, 'index.html')
+    posts = Post.objects.filter(estado=True)
+    data = {
+        'posts': posts
+    }
+    return render(request, 'index.html', data)
+
+
+def detallePost(request, slug):
+    posts = Post.objects.get(slug=slug)
+
+    data = {
+        'detalle_posts': posts
+    }
+    # post = get_object_or_404(Post, slug=slug) # Funciona igual que la consulta, pero valida si existe o no
+    return render(request, 'post.html', data)
+
+
+def generales(request):
+    posts = Post.objects.filter(
+        estado=True,
+        categoria=Categoria.objects.get(nombre='Generales'))
+
+    data = {
+        'posts': posts
+    }
+    return render(request, 'generales.html', data)
+
+
+def programacion(request):
+    posts = Post.objects.filter(
+        estado=True,
+        categoria=Categoria.objects.get(nombre='Programación'))
+
+    data = {
+        'posts': posts
+    }
+    return render(request, 'programacion.html', data)
+
+
+def tutoriales(request):
+    posts = Post.objects.filter(
+        estado=True,
+        categoria=Categoria.objects.get(nombre='Tutoriales')
+    )
+
+    data = {
+        'posts': posts
+    }
+
+    return render(request, 'tutoriales.html', data)
+
+
+def tecnologia(request):
+    posts = Post.objects.filter(
+        estado=True,
+        categoria=Categoria.objects.get(nombre='Tecnología')
+    )
+
+    data = {
+        'posts': posts
+    }
+    return render(request, 'tecnologia.html', data)
+
+
+def video_juegos(request):
+    posts = Post.objects.filter(
+        estado=True,
+        categoria=Categoria.objects.get(nombre='Video juegos')
+    )
+
+    data = {
+        'posts': posts
+    }
+    return render(request, 'videojuegos.html', data)
