41d48ac2470ca2178c00a5a7c67f83ec7ccd8a39
drakodarpan81 <drakodarpan@gmail.com>
2022-04-07 10:52:35 -0600

    Add paginator - 33

--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def Home(request):
+    queryset = request.GET.get("buscar")
     posts = Post.objects.filter(estado=True)
+
+    if queryset:
+        posts = Post.objects.filter(
+            Q(titulo__icontains=queryset) |
+            Q(descripcion__icontains=queryset)
+        ).distinct()
+
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
     data = {
         'posts': posts
     }
@@ -11,19 +25,27 @@ def Home(request):
 
 
 def detallePost(request, slug):
-    posts = Post.objects.get(slug=slug)
-
-    data = {
-        'detalle_posts': posts
-    }
-    # post = get_object_or_404(Post, slug=slug) # Funciona igual que la consulta, pero valida si existe o no
-    return render(request, 'post.html', data)
+    # Funciona igual que la consulta, pero valida si existe o no
+    posts = get_object_or_404(Post, slug=slug)
+    return render(request, 'post.html', {'detalle_posts': posts})
 
 
 def generales(request):
+    queryset = request.GET.get("buscar")
     posts = Post.objects.filter(
         estado=True,
-        categoria=Categoria.objects.get(nombre='Generales'))
+        categoria=Categoria.objects.get(nombre__iexact='Generales'))
+
+    if queryset:
+        posts = Post.objects.filter(
+            Q(titulo__icontains=queryset) |
+            Q(descripcion__icontains=queryset),
+            estado=True,
+            categoria=Categoria.objects.get(nombre__iexact='Generales'))
+
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
 
     data = {
         'posts': posts
@@ -32,9 +54,21 @@ def generales(request):
 
 
 def programacion(request):
+    queryset = request.GET.get("buscar")
     posts = Post.objects.filter(
         estado=True,
-        categoria=Categoria.objects.get(nombre='Programación'))
+        categoria=Categoria.objects.get(nombre__iexact='Programación'))
+
+    if queryset:
+        posts = Post.objects.filter(
+            Q(titulo__icontains=queryset) |
+            Q(descripcion=queryset),
+            estado=True,
+            categoria=Categoria.objects.get(nombre__iexact='Programación'))
+
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
 
     data = {
         'posts': posts
@@ -43,10 +77,23 @@ def programacion(request):
 
 
 def tutoriales(request):
+    queryset = request.Get.get("buscar")
     posts = Post.objects.filter(
         estado=True,
-        categoria=Categoria.objects.get(nombre='Tutoriales')
+        categoria=Categoria.objects.get(nombre__iexact='Tutoriales')
     )
+
+    if queryset:
+        posts = Post.objects.filter(
+            Q(titulo__icontains=queryset) |
+            Q(descripcion=queryset),
+            estado=True,
+            categoria=Categoria.objects.get(nombre__iexact='Tutoriales')
+        )
+
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
 
     data = {
         'posts': posts
@@ -56,10 +103,23 @@ def tutoriales(request):
 
 
 def tecnologia(request):
+    queryset = request.Get.get("buscar")
     posts = Post.objects.filter(
         estado=True,
-        categoria=Categoria.objects.get(nombre='Tecnología')
+        categoria=Categoria.objects.get(nombre__iexact='Tecnología')
     )
+
+    if queryset:
+        posts = Post.objects.filter(
+            Q(titulo__icontains=queryset) |
+            Q(descripcion=queryset),
+            estado=True,
+            categoria=Categoria.objects.get(nombre__iexact='Tecnología')
+        )
+
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
 
     data = {
         'posts': posts
@@ -68,11 +128,24 @@ def tecnologia(request):
 
 
 def video_juegos(request):
+    queryset = request.GET.get("buscar")
     posts = Post.objects.filter(
         estado=True,
-        categoria=Categoria.objects.get(nombre='Video juegos')
+        categoria=Categoria.objects.get(nombre__iexact='Video juegos')
     )
 
+    if queryset:
+        posts = Post.objects.filter(
+            Q(titulo__icontains=queryset) |
+            Q(descripcion=queryset),
+            estado=True,
+            categoria=Categoria.objects.get(nombre__iexact='Video juegos')
+        )
+
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    
     data = {
         'posts': posts
     }
